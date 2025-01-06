be3de4049640af453020c88575b0365c10382536
Sanjana Sidgiddi <sanja@mac.mynetworksettings.com>
2025-01-06 16:40:49 -0500

    Added Course Component

---
+++ trivia-front-end/src/components/Course/Course.tsx
@@ -0,0 +1,32 @@
+
+function Course() {
+
+    /** From the Student Home page, we can click on a particular course and redirect to here. */
+    return (
+      <div>
+        <h1>Course Information Page</h1>
+        <table>
+            <tr>
+                <th>Name -- </th>
+                <td>Insert name</td>
+            </tr>
+            <tr>
+                <th>Description -- </th>
+                <td>Insert Description</td>
+            </tr>
+            <tr>
+                <th>Educator -- </th>
+                <td>Insert </td>
+            </tr>
+            {/* <tr>
+                <th>Fee -- </th>
+                <td>Insert </td>
+            </tr> */}
+            
+        </table>
+
+      </div>
+    )
+  }
+  
+  export default Course
