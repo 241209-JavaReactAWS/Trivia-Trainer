3ab987d09bc8ecb2fb8e38e80b887730c36e8c0d
Rakesh Chandraraj <crakesh20@vt.edu>
2025-01-13 14:19:30 -0500

    Trying to sync full project

--- trivia-front-end/src/components/interfaces/Educator.ts
+++ trivia-front-end/src/components/interfaces/Educator.ts
@@ -1,8 +1,11 @@
 /* Object to represent the backend model class Educator */
 import { Course } from "./Course";
+import { User } from "./User";
 
 export interface Educator {
     educatorId: number,
     details: string,
-    courses: Course[]
+    courses: Course[],
+    // Make an optional field that accepts a user type
+    user?: User
 }
